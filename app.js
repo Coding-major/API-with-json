0156b4b6e9c96b022c04612378c852c3c19e4063
Shehu Mustapha <shehumustapham@gmail.com>
2022-12-27 20:19:16 +0100

    query using json

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const products = require("./data");
 
 const app = express();
 // app.use(express.static("public"))
+app.get("/api/products", (req, res) => {
+    const productsDetails = products.map((result) => {
+        const {name, id, image} = result;
+        return {id, name, image}
+    })
 
+    res.json(productsDetails)
+})
+
+app.get("/", (req, res) => {
+    res.send('<h1> Home page</h1><a href="/api/products">products</a>')
+})
 app.get("/api/products/:productId", (req, res) => {
     const productId = Number(req.params.productId)
     const finded = products.find(
@@ -15,9 +26,33 @@ app.get("/api/products/:productId", (req, res) => {
     }
 
     return res.json(finded)
-
-
+    console.log(req.params);
 })
+
+app.get("/api/products/:productId/review/:reviewId", (req, res) => {
+    res.send("hello world")
+})
+
+app.get("/api/v1/query", function(req, res) {
+    const {search, limit} = req.query
+    let sortedProducts = [...products]
+    
+    if (search) {
+        sortedProducts = sortedProducts.filter((result) => {
+            return result.name.startsWith(search)
+        })
+    }
+
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+
+    if (sortedProducts.length<1) {
+        return res.status(200).send("No products matches your search")
+    }
+    res.status(200).json(sortedProducts)
+})
+
 
 app.listen(3000, () => {
      console.log("connection started at port 3000")
